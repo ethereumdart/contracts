9723913b321e480e308527ef9f01226bea228654
Simon Binder <simolus3@gmail.com>
2019-05-04 22:06:12 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+A library for Dart developers.
+
+TBD
---
+++ example/contracts_example.dart
@@ -0,0 +1,6 @@
+import 'package:contracts/contracts.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
